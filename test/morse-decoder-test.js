ae1fe56c8464ac41fd59d4624825edb89cd5e3c6
Alessio Coser <alessio.coser@xpeppers.com>
2017-12-22 14:23:52 +0100

    remove function keyword and use lambda functions in tests

--- test/morse-decoder-test.js
+++ test/morse-decoder-test.js
@@ -2,24 +2,24 @@ const { equal } = require('assert')
 const { Morse } = require('../morse-decoder')
 let morse = new Morse()
 
-test('can translate a letter from morse to Text', function () {
+test('can translate a letter from morse to Text', () => {
   equal(morse.decode('.-'), 'A')
   equal(morse.decode('--.-'), 'Q')
 })
 
-test('can translate more letters using char separator', function () {
+test('can translate more letters using char separator', () => {
   equal(morse.decode('.- --.-'), 'AQ')
   equal(morse.decode('.- --.- .-'), 'AQA')
 })
 
-test('can translate more words using triple char separator', function () {
+test('can translate more words using triple char separator', () => {
   equal(morse.decode('.- --.-   .- --.-'), 'AQ AQ')
 })
 
-test('spaces after and before morse code are trimmed', function () {
+test('spaces after and before morse code are trimmed', () => {
   equal(morse.decode('   .- --.-   .- --.- '), 'AQ AQ')
 })
 
-test('Example from description', function () {
+test('Example from description', () => {
   equal(morse.decode('.... . -.--   .--- ..- -.. .'), 'HEY JUDE')
 })
