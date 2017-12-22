3d42cd847e317e1a046beb77ce004036bfc2dfb6
Alessio Coser <alessio.coser@xpeppers.com>
2017-12-22 12:59:07 +0100

    renames methods

--- morse-decoder.js
+++ morse-decoder.js
@@ -8,23 +8,23 @@ class Morse {
   }
 
   decode (code) {
-    return this.toMorseWords(code).map(this.decodeWord.bind(this)).join(this.ASCII_WORD_SEPARATOR)
+    return this.toWords(code).map(this.decodeWord.bind(this)).join(this.ASCII_WORD_SEPARATOR)
   }
 
-  decodeWord (morseWord) {
-    return this.toMorseChars(morseWord).map(this.decodeChar.bind(this)).join(this.ASCII_CHAR_SEPARATOR)
-  }
-
-  toMorseChars (string) {
-    return string.trim().split(this.MORSE_CHAR_SEPARATOR)
-  }
-
-  toMorseWords (string) {
-    return string.trim().split(this.MORSE_WORD_SEPARATOR)
+  decodeWord (word) {
+    return this.toChars(word).map(this.decodeChar.bind(this)).join(this.ASCII_CHAR_SEPARATOR)
   }
 
   decodeChar (morseChar) {
     return this.CODES[morseChar]
+  }
+
+  toWords (string) {
+    return string.trim().split(this.MORSE_WORD_SEPARATOR)
+  }
+
+  toChars (string) {
+    return string.trim().split(this.MORSE_CHAR_SEPARATOR)
   }
 }
 
