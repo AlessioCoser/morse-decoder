6c25576668cc9b23b201765b012e52cce7f2f5ad
Alessio Coser <alessio.coser@xpeppers.com>
2017-12-22 12:15:11 +0100

    extracts decodeChars

--- morse-decoder.js
+++ morse-decoder.js
@@ -1,12 +1,14 @@
 const MORSE_CODES = require('./lib/morse-codes')
+const CHAR_SEPARATOR = ' '
 
 function decodeMorse (morseCode) {
-  let chars = morseCode.split(' ')
+  let chars = morseCode.split(CHAR_SEPARATOR)
 
-  return chars.reduce((text, char) => {
-    text += MORSE_CODES[char]
-    return text
-  }, '')
+  return decodeChars(chars)
+}
+
+function decodeChars (chars) {
+  return chars.reduce((text, char) => text + MORSE_CODES[char], String.prototype)
 }
 
 module.exports = { decodeMorse }
