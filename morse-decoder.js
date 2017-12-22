fe8c900bdd0ccf1b4da000b041d2d9042b839497
Alessio Coser <alessio.coser@xpeppers.com>
2017-12-22 12:09:13 +0100

    translate more letters

--- morse-decoder.js
+++ morse-decoder.js
@@ -1,7 +1,12 @@
 const MORSE_CODES = require('./lib/morse-codes')
 
 function decodeMorse (morseCode) {
-  return MORSE_CODES[morseCode]
+  let chars = morseCode.split(' ')
+
+  return chars.reduce((text, char) => {
+    text += MORSE_CODES[char]
+    return text
+  }, '')
 }
 
 module.exports = { decodeMorse }
--- test/morse-decoder-test.js
+++ test/morse-decoder-test.js
@@ -6,6 +6,11 @@ test('can translate a letter from morse to Text', function () {
   equal(decodeMorse('--.-'), 'Q')
 })
 
+test('can translate more letters using char separator', function () {
+  equal(decodeMorse('.- --.-'), 'AQ')
+  equal(decodeMorse('.- --.- .-'), 'AQA')
+})
+
 test.skip('Example from description', function () {
   equal(decodeMorse('.... . -.--   .--- ..- -.. .'), 'HEY JUDE')
 })
