887263d6f6f29d0014cd66db0b701f4b91559f4a
Alessio Coser <alessio.coser@xpeppers.com>
2018-01-10 17:40:18 +0100

    can decode 111 to "-"

--- test/binary-morse-decoder-test.js
+++ test/binary-morse-decoder-test.js
@@ -6,4 +6,8 @@ test('BinaryMorse', () => {
   test('can decode 1 to "."', () => {
     equal(binaryMorse.decode('1'), '.')
   })
+
+  test('can decode 111 to "-"', () => {
+    equal(binaryMorse.decode('111'), '-')
+  })
 })
