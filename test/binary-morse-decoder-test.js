579cca2eec2d21e8f1c6231e19f54f0706eebf7d
Alessio Coser <alessio.coser@xpeppers.com>
2018-06-05 14:38:46 +0200

    adds a test in order to handle the real morse decoder

--- test/binary-morse-decoder-test.js
+++ test/binary-morse-decoder-test.js
@@ -1,36 +1,46 @@
 const { equal } = require('assert')
 const { BinaryMorse } = require('../lib/binary-morse-decoder')
+const { Morse } = require('../lib/morse-decoder')
 let binaryMorse = new BinaryMorse()
 
-test('BinaryMorse', () => {
-  test('can decode 1 to "."', () => {
+describe('BinaryMorse', () => {
+  it('can decode 1 to "."', () => {
     equal(binaryMorse.decode('1'), '.')
   })
 
-  test('can decode 111 to "-"', () => {
+  it('can decode 111 to "-"', () => {
     equal(binaryMorse.decode('111'), '-')
   })
 
-  test('can decode pause between symbols', () => {
+  it('can decode pause between symbols', () => {
     equal(binaryMorse.decode('10111'), '.-')
   })
 
-  test('can decode pause between chars', () => {
+  it('can decode pause between chars', () => {
     equal(binaryMorse.decode('10111000111010101'), '.- -...')
   })
 
-  test('can decode space between words', () => {
+  it('can decode space between words', () => {
     equal(binaryMorse.decode('10111000111010101000000010111'), '.- -...   .-')
   })
 
-  test('can decode in different sample rate', () => {
+  it('can decode in different sample rate', () => {
     equal(binaryMorse.decode('1100111111000000111111001100110011000000000000001100111111'), '.- -...   .-')
   })
 
-  test('cand decode HEY JUDE from binary', () => {
+  it('can decode HEY JUDE from binary', () => {
     let binary = '1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011'
-    let expectedMorseCode = '.... . -.--   .--- ..- -.. .'
+    equal(binaryMorse.decode(binary), '.... . -.--   .--- ..- -.. .')
 
-    equal(binaryMorse.decode(binary), expectedMorseCode)
+    let morse = new Morse()
+    equal(morse.decode(binaryMorse.decode(binary)), 'HEY JUDE')
+  })
+
+  it('can decode REAL morse code', () => {
+    let binary = '110011001100110000001100000011111100110011111100111111000000000011001111110011111100111111000000110011001111110000001111110011001100000011'
+    equal(binaryMorse.decodeAdvanced(binary), '.... . -.--   .--- ..- -.. .')
+
+    let morse = new Morse()
+    equal(morse.decode(binaryMorse.decodeAdvanced(binary)), 'HEY JUDE')
   })
 })
--- test/morse-decoder-test.js
+++ test/morse-decoder-test.js
@@ -2,26 +2,26 @@ const { equal } = require('assert')
 const { Morse } = require('../lib/morse-decoder')
 let morse = new Morse()
 
-test('Morse', () => {
-  test('can translate a letter from morse to Text', () => {
+describe('Morse', () => {
+  it('can translate a letter from morse to Text', () => {
     equal(morse.decode('.-'), 'A')
     equal(morse.decode('--.-'), 'Q')
   })
 
-  test('can translate more letters using char separator', () => {
+  it('can translate more letters using char separator', () => {
     equal(morse.decode('.- --.-'), 'AQ')
     equal(morse.decode('.- --.- .-'), 'AQA')
   })
 
-  test('can translate more words using triple char separator', () => {
+  it('can translate more words using triple char separator', () => {
     equal(morse.decode('.- --.-   .- --.-'), 'AQ AQ')
   })
 
-  test('spaces after and before morse code are trimmed', () => {
+  it('spaces after and before morse code are trimmed', () => {
     equal(morse.decode('   .- --.-   .- --.- '), 'AQ AQ')
   })
 
-  test('Example from description', () => {
+  it('Example from description', () => {
     equal(morse.decode('.... . -.--   .--- ..- -.. .'), 'HEY JUDE')
   })
 })
